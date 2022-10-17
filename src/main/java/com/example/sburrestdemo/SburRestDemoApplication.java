dd1e0a01940d5492d6a1ae1fbe67ba30cbd9d769
icytailz <icy.tailz@gmail.com>
2022-10-17 04:38:07 +0000

    add HTTP methods

--- src/main/java/com/example/sburrestdemo/SburRestDemoApplication.java
+++ src/main/java/com/example/sburrestdemo/SburRestDemoApplication.java
@@ -1,9 +1,88 @@
 package com.example.sburrestdemo;
 
+import java.io.ObjectInputStream.GetField;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
+
+import javax.swing.text.html.Option;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.IdGenerator;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/coffees")
+class RestApiDemoController {
+	private List<Coffee> coffees = new ArrayList<>();
+
+	public	RestApiDemoController() {
+		coffees.addAll(List.of(
+			new Coffee("Cafe Gereza"),
+			new Coffee("Cafe Ganador"),
+			new Coffee("Cafe Lareno"),
+			new Coffee("Cafe Tres Pontas")
+		));
+	}
+
+	// @RequestMapping(value = "/coffees", method = RequestMethod.GET)
+	// Iterable<Coffee> getCoffees(){
+	// 	return coffees;
+	// }
+
+	@GetMapping
+	Iterable<Coffee> getCoffees() {
+		return coffees;
+	}
+
+	@GetMapping("/{id}")
+	Optional<Coffee> getCoffeeById(@PathVariable String id) {
+	 for (Coffee c: coffees) {
+		if (c.getId().equals(id)) {
+			return Optional.of(c);
+		}
+	 }
+	 return Optional.empty();
+	}
+
+	@PostMapping
+	Coffee postCoffee(@RequestBody Coffee coffee) {
+		coffees.add(coffee);
+		return coffee;
+	}
+
+	@PutMapping("/{id}")
+	ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
+		int coffeeIndex = -1;
+
+		for (Coffee c: coffees) {
+			if (c.getId().equals(id)) {
+				coffeeIndex = coffees.indexOf(c);
+				coffees.set(coffeeIndex, coffee);
+			}
+		}
+		return (coffeeIndex == -1) ? 
+				new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED) :
+				new ResponseEntity<>(coffee, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{id}")
+	void deleteCoffee(@PathVariable String id) {
+		coffees.removeIf(C -> c.getId().euqals(id));
+	}
+}
+
 
 @SpringBootApplication
 public class SburRestDemoApplication {
@@ -15,7 +94,7 @@ public class SburRestDemoApplication {
 }
 
 class Coffee {
-	private final String id; //final mean the attribute can only be assigned only once and never modified
+	private final String id; //final means the attribute can only be assigned only once and never modified
 	private String name;
 
 	public Coffee(String id, String name) {
