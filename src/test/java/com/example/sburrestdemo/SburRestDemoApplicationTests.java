ee40ef81d051b6c0900c4c93da730b74e3bb07b8
icytailz <65644499+icytailz@users.noreply.github.com>
2022-10-17 10:10:38 +0700

    Update SburRestDemoApplicationTests.java
    add class

--- src/test/java/com/example/sburrestdemo/SburRestDemoApplicationTests.java
+++ src/test/java/com/example/sburrestdemo/SburRestDemoApplicationTests.java
@@ -11,3 +11,29 @@ class SburRestDemoApplicationTests {
 	}
 
 }
+
+class Coffee {
+	private final String id; //final mean the attribute can only be assigned only once and never modified
+	private String name;
+
+	public Coffee(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Coffee(string name) {
+		this(UUID.randomUUID().toString(), name); //create a unique id if none provided upon creation of Coffee
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
