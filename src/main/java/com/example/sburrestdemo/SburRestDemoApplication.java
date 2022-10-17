7c37a88420fc7f311f3c8ea7cf9f024d90cffec8
Icy Tailz <icy.tailz@gmail.com>
2022-10-17 17:40:38 +0700

    finish chapter 4

--- src/main/java/com/example/sburrestdemo/SburRestDemoApplication.java
+++ src/main/java/com/example/sburrestdemo/SburRestDemoApplication.java
@@ -1,15 +1,19 @@
 package com.example.sburrestdemo;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import javax.annotation.PostConstruct;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,64 +23,58 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/coffees")
-class RestApiDemoController {
-	private List<Coffee> coffees = new ArrayList<>();
+@Component
+class DataLoader {
+	private final CoffeeRepository coffeeRepository;
 
-	public	RestApiDemoController() {
-		coffees.addAll(List.of(
+	public DataLoader(CoffeeRepository coffeeRepository) {
+		this.coffeeRepository = coffeeRepository;
+	}
+	@PostConstruct
+	private void loadData() {
+		coffeeRepository.saveAll(List.of(
 			new Coffee("Café Cereza"),
 			new Coffee("Café Ganador"),
 			new Coffee("Café Lareño"),
 			new Coffee("Café Três Pontas")
 		));
 	}
+}
 
-	// @RequestMapping(value = "/coffees", method = RequestMethod.GET)
-	// Iterable<Coffee> getCoffees(){
-	// 	return coffees;
-	// }
+@RestController
+@RequestMapping("/coffees")
+class RestApiDemoController {
+	private final CoffeeRepository coffeeRepository;
 
+	public	RestApiDemoController(CoffeeRepository coffeeRepository) {
+		this.coffeeRepository = coffeeRepository;
+	}
+	
 	@GetMapping
 	Iterable<Coffee> getCoffees() {
-		return coffees;
+		return coffeeRepository.findAll();
 	}
 
 	@GetMapping("/{id}")
 	Optional<Coffee> getCoffeeById(@PathVariable String id) {
-	 for (Coffee c: coffees) {
-		if (c.getId().equals(id)) {
-			return Optional.of(c);
+			return coffeeRepository.findById(id);
 		}
-	 }
-	 return Optional.empty();
-	}
 
 	@PostMapping
 	Coffee postCoffee(@RequestBody Coffee coffee) {
-		coffees.add(coffee);
-		return coffee;
+		return coffeeRepository.save(coffee);
 	}
 
 	@PutMapping("/{id}")
 	ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
-		int coffeeIndex = -1;
-
-		for (Coffee c: coffees) {
-			if (c.getId().equals(id)) {
-				coffeeIndex = coffees.indexOf(c);
-				coffees.set(coffeeIndex, coffee);
-			}
-		}
-		return (coffeeIndex == -1) ? 
-				new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED) :
-				new ResponseEntity<>(coffee, HttpStatus.OK);
+		return (coffeeRepository.existsById(id)) 
+				? new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.OK)
+				: new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.CREATED);
 	}
 
 	@DeleteMapping("/{id}")
 	void deleteCoffee(@PathVariable String id) {
-		coffees.removeIf(c -> c.getId().equals(id));
+		coffeeRepository.deleteById(id);
 	}
 }
 
@@ -89,10 +87,16 @@ public class SburRestDemoApplication {
 	}
 
 }
+interface CoffeeRepository extends CrudRepository<Coffee, String> {}
 
+@Entity
 class Coffee {
-	private final String id; //final means the attribute can only be assigned only once and never modified
+	@Id
+	private String id;
 	private String name;
+
+	public Coffee() {
+	}
 
 	public Coffee(String id, String name) {
 		this.id = id;
@@ -105,6 +109,10 @@ class Coffee {
 
 	public String getId() {
 		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 
 	public String getName() {
